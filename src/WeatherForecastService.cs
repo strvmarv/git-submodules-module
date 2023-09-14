a35c2ad7c5f7b25908dfa2b5507abca2dec7b00d
Paul Marvin <paul.marvin@radancy.com>
2023-09-14 10:40:22 -0500

    Add module contents

---
+++ src/WeatherForecastService.cs
@@ -0,0 +1,28 @@
+﻿namespace ExampleModule
+{
+    public class WeatherForecastService
+    {
+        public static List<string> Summaries = new()
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public WeatherForecast[] GetWeatherForecast()
+        {
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                Summaries[Random.Shared.Next(Summaries.Count)]
+            ))
+            .ToArray();
+            return forecast;
+        }
+    }
+
+    public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
